47cfa9079b7721b75009b768fde76004c5c54b19
Some1else{} <some1lambda@somelambda.dev>
2025-07-10 14:47:20 +0200

    Made strings more concise

--- lua/starfall/libs_cl/pac3.lua
+++ lua/starfall/libs_cl/pac3.lua
@@ -5,8 +5,8 @@ local PAC = pace
 local checkluatype = SF.CheckLuaType
 local registerprivilege = SF.Permissions.registerPrivilege
 
-registerprivilege("pac3.load", "Load a PAC3", "Allows the user load a PAC3", { client = { default = 1 } })
-registerprivilege("pac3.clear", "Clear your PAC3", "Allows the user to clear the PAC3", { client = { default = 1 } })
+registerprivilege("pac3.load", "Load a PAC3 outfit", "Allows the user load a PAC3", { client = { default = 1 } })
+registerprivilege("pac3.clear", "Clear your PAC3 outfit", "Allows the user to clear the PAC3", { client = { default = 1 } })
 
 --- PAC3 library
 -- @name pac3
@@ -19,7 +19,7 @@ local checkpermission = instance.player ~= SF.Superuser and SF.Permissions.check
 
 local pac3_library = instance.Libraries.pac3
 
---- Loads a PAC3
+--- Loads a PAC3 outfit from a string
 -- @param string str Data to load
 -- @param boolean? clear Whether to clear the current parts
 function pac3_library.loadFromString(str,clear)
@@ -29,7 +29,7 @@ function pac3_library.loadFromString(str,clear)
     if clear == true then
         checkpermission(instance,nil,"pac3.clear")
     end
-    
+
     local data,_ = PAC.luadata.Decode(str)
     if data then
         PAC.LoadPartsFromTable(data,clear)
