a4404cd0774e7262ecbd2c87854bff86e8dd8a53
Some1else{} <some1lambda@somelambda.dev>
2025-07-10 17:09:46 +0200

    Added table load

--- lua/starfall/libs_cl/pac3.lua
+++ lua/starfall/libs_cl/pac3.lua
@@ -14,14 +14,16 @@ registerprivilege("pac3.clear", "Clear your PAC3 outfit", "Allows the user to cl
 -- @libtbl pac3_library
 SF.RegisterLibrary("pac3")
 
+local decode = pace.luadata.Decode
+
 return function(instance)
 local checkpermission = instance.player ~= SF.Superuser and SF.Permissions.check or function() end
 local pac3_library = instance.Libraries.pac3
 
 --- Loads a PAC3 outfit from its filename
--- @param string name The filename
+-- @param string name The outfit's filename
 -- @param boolean? clear Whether to clear the current outfit
-function pac3_library.loadParts(name,clear)
+function pac3_library.loadOutift(name,clear)
 	checkpermission(instance,nil,"pac3.load")
 	if clear == true then
         checkpermission(instance,nil,"pac3.clear")
@@ -32,21 +34,27 @@ function pac3_library.loadParts(name,clear)
 	PAC.LoadParts(name,clear)
 end
 
---- Loads a PAC3 outfit from a string
--- @param string str Data to load
+--- Loads a PAC3 outfit from a table or a string
+-- @param string|table data Data to load
 -- @param boolean? clear Whether to clear the current outfit
-function pac3_library.loadFromString(str,clear)
+function pac3_library.loadFromData(data,clear)
     checkpermission(instance,nil,"pac3.load")
 	if clear == true then
         checkpermission(instance,nil,"pac3.clear")
     end
 
-    checkluatype(str,TYPE_STRING)
+    if istable(data) then
+		PAC.LoadPartsFromTable(data,clear)
+	elseif isstring(data) then
+		local pac_data,_ = decode(str)
+		if pac_data then
+			PAC.LoadPartsFromTable(pac_data,clear)
+		end
+	else
+		SF.ThrowTypeError("table or string", SF.GetType(data), 2)
+	end
 
-    local data,_ = PAC.luadata.Decode(str)
-    if data then
-        PAC.LoadPartsFromTable(data,clear)
-    end
+
 end
 
 --- Clears the PAC3 outfit
